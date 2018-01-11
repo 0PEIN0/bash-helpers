b5ecbe95d274c31a008b93e337e0260072f226bc
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-01-11 18:48:32 +0600

    Updated ssh related operations.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- src/bash/system.sh
+++ src/bash/system.sh
@@ -306,12 +306,12 @@ sshOperationsSudo() {
   if [ "${?}" = "0" ] ; then
     return
   fi;
-  ssh_agent_verify
-  ssh_agent_add_root
-  github_keyscan_sudo
-  bitbucket_keyscan_sudo
-  github_auth
-  bitbucket_auth
+  eval "$(ssh-agent -s)"
+  ssh-add /root/.ssh/id_rsa
+  ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+  ssh-keyscan -t rsa bitbucket.com >> /root/.ssh/known_hosts
+  printf "yes\n" | ssh -T git@github.com
+  printf "yes\n" | ssh -T git@bitbucket.com
 }
 
 sshOperationsNonSudo() {
@@ -320,12 +320,12 @@ sshOperationsNonSudo() {
   if [ "${?}" = "0" ] ; then
     return
   fi;
-  ssh_agent_verify
-  ssh_agent_add
-  github_keyscan_non_sudo
-  bitbucket_keyscan_non_sudo
-  github_auth
-  bitbucket_auth
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+  ssh-keyscan -t rsa github.com >> $SYSTEM_ROOT_FOLDER/.ssh/known_hosts
+  ssh-keyscan -t rsa bitbucket.com >> $SYSTEM_ROOT_FOLDER/.ssh/known_hosts
+  printf "yes\n" | ssh -T git@github.com
+  printf "yes\n" | ssh -T git@bitbucket.com
 }
 
 systemUpdatesNonSudo() {
