2e21b586243f7681b8014842f90fd67052e929fd
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-02-13 18:27:11 +0600

    Updated npm related aliases.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- src/bash/nodejs.sh
+++ src/bash/nodejs.sh
@@ -88,6 +88,7 @@ nodeExpressNpmInitiation() {
 
 alias node_update=nodeUpdates
 alias install_nodejs=installNodejs
-alias npm_adduser="npm adduser"# asks for username, password and public-email in the prompt
-alias npm_publish="npm publish"# don't forget to upgrade the version before publishing
+alias npm_adduser="npm adduser" # asks for username, password and public-email in the prompt
+alias npm_i="npm install"
+alias npm_publish="npm publish" # don't forget to upgrade the version before publishing
 alias npm_whoami="npm whoami"
