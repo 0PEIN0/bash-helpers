9e35313804c82d8705a4c4a1372ad294f2d813dd
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-05 16:33:21 +0600

    Updated git pull for all repositories python script.

--- src/python/git_pull_all.py
+++ src/python/git_pull_all.py
@@ -1,8 +1,17 @@
 import getpass
 import subprocess
 
+LOCAL_SETTINGS = {}
+
+try:
+    from local_settings import LOCAL_SETTINGS
+except Exception as ex:
+    pass
+
 
 def execute_shell_command(cmd):
+    cmd = '. {bash_alias_path}; source {bash_alias_path};'.format(
+        bash_alias_path=LOCAL_SETTINGS['BASH_ALIASES_FILE_PATH']) + cmd
     std_response = subprocess.Popen(cmd,
                                     shell=True,
                                     executable='/bin/zsh',
@@ -12,7 +21,7 @@ def execute_shell_command(cmd):
 
 
 system_user_name = getpass.getuser()
-git_folder_path = 'cd /home/{system_user_name}/Gitrepos/'.format(
+git_folder_path = 'cd $SYSTEM_ROOT_GIT_REPO_FOLDER/'.format(
     system_user_name=system_user_name)
 project_folder_list = execute_shell_command(
     cmd='{git_folder_path} && ls -l'.format(git_folder_path=git_folder_path))
@@ -26,7 +35,7 @@ for project_folder in project_folder_list:
         git_folder_path=git_folder_path,
         project_name=project_name)
     res = execute_shell_command(
-        cmd='{project_folder_path} && git fetch --all && branchName="$(git rev-parse --abbrev-ref HEAD)" && git reset --hard origin/'.format(project_folder_path=project_folder_path) + '"${branchName}"')
+        cmd='{project_folder_path} && printf "yes\n" | git fetch --all && gitResetHard'.format(project_folder_path=project_folder_path))
     branch_name = execute_shell_command(
         cmd='{project_folder_path} && git branch'.format(project_folder_path=project_folder_path))
     branch_name = branch_name[0].replace('* ', '')
---
+++ src/python/sample_local_settings.py
@@ -0,0 +1,3 @@
+CONFIG = {
+    'BASH_ALIASES_FILE_PATH': '~/bash-aliases.sh'
+}
