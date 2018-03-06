2209129865176053451ca3e7025918843300bbfb
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-06 18:14:57 +0600

    Added support to clone my other repositories.

--- src/bash/utils.sh
+++ src/bash/utils.sh
@@ -29,6 +29,11 @@ startGoogleChrome() {
 alias android="sh $SYSTEM_APPS_FOLDER/android-studio/bin/studio.sh"
 alias apache_reload='/etc/init.d/apache2 reload'
 alias atom_up=apmUpdates
+alias clone_crugen="cd $SYSTEM_ROOT_GIT_REPO_FOLDER && git clone git@github.com:0PEIN0/crugen.git"
+alias clone_jslizer="cd $SYSTEM_ROOT_GIT_REPO_FOLDER && git clone git@github.com:0PEIN0/jslizer.git"
+alias clone_jslizer_demo="cd $SYSTEM_ROOT_GIT_REPO_FOLDER && git clone git@github.com:0PEIN0/jslizer-demo.git"
+alias clone_autogger="cd $SYSTEM_ROOT_GIT_REPO_FOLDER && git clone git@github.com:0PEIN0/autogger.git"
+alias clone_api_exec="cd $SYSTEM_ROOT_GIT_REPO_FOLDER && git clone git@github.com:0PEIN0/api-exec.git"
 alias fb_d='firebase deploy'
 alias fb_i='firebase init'
 alias fb_l='firebase list'
