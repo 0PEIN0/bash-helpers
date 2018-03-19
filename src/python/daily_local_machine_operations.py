71f6622bc0a400d36081651e6e65c61253900cad
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-19 14:54:33 +0600

    Updated daily local machine operation script.

--- src/python/daily_local_machine_operations.py
+++ src/python/daily_local_machine_operations.py
@@ -1,4 +1,5 @@
 from file_helper import FileHelper
+from git_operations import GitOperations
 from local_settings_loader import LocalSettingsLoader
 from youtube_music_video_sync import YoutubeMusicVideoSync
 
@@ -8,4 +9,8 @@ class DailyLocalMachineOperations(object):
     def __init__(self):
         FileHelper().copy_and_replace_files(
             file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST'])
-        # YoutubeMusicVideoSync().sync_mvs()
+        YoutubeMusicVideoSync().sync_mvs()
+        GitOperations().fetch_all_repos_and_reset_hard()
+
+
+DailyLocalMachineOperations()
--- src/python/git_fetch_all_repo.py
+++
@@ -1,3 +0,0 @@
-from git_operations import GitOperations
-
-GitOperations().fetch_all_repos_and_reset_hard()
