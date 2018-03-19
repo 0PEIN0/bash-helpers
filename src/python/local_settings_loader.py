9006bc73bc2ed1a1201d4f59352acb5027aff958
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-19 22:08:44 +0600

    Initialized some local settings properties in case some of their value is not supplied.

--- src/python/local_settings_loader.py
+++ src/python/local_settings_loader.py
@@ -9,3 +9,9 @@ class LocalSettingsLoader(object):
             print(
                 'FATAL ERROR: could not import local settings file. Are you sure the `local_settings.py` file exists?')
         self.LOCAL_SETTINGS = LOCAL_SETTINGS
+        self._initializer_empty_lists()
+
+    def _initializer_empty_lists(self):
+        for item in ['FILE_REPLACE_DEF_LIST', 'YOUTUBE_VIDEO_ID_LIST', ]:
+            if self.LOCAL_SETTINGS[item] is None:
+                self.LOCAL_SETTINGS[item] = []
