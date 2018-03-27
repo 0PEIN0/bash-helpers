7dc3adacd99e2326bd2ac820a6f657c3df00d7f3
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-27 11:29:29 +0600

    Extended support for file replace daily operation.

--- src/python/daily_local_machine_operations.py
+++ src/python/daily_local_machine_operations.py
@@ -8,9 +8,11 @@ class DailyLocalMachineOperations(object):
 
     def __init__(self):
         FileHelper().copy_and_replace_files(
-            file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST'])
+            file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST_BEFORE_DAILY_OPERATION'])
         YoutubeMusicVideoSync().sync_mvs()
         GitOperations().fetch_all_repos_and_reset_hard()
+        FileHelper().copy_and_replace_files(
+            file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST_AFTER_DAILY_OPERATION'])
 
 
 DailyLocalMachineOperations()
