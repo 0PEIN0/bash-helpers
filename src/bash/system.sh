cb1360ec0ee80cddaaea314dfe154dca5c267807
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-03-29 15:06:27 +0600

    Updated alias name for apt get upgrade.

--- src/bash/system.sh
+++ src/bash/system.sh
@@ -385,7 +385,7 @@ pullBashHelpersRepo() {
 alias admin='sudo su'
 alias allow_port_sudo='sudo ufw allow '
 alias apache_reload='/etc/init.d/apache2 reload'
-alias apt_get=aptGetUpgrade
+alias apt_get_upgrade=aptGetUpgrade
 alias bash_refresh=bashRefresh
 alias bash_helpers_fetch=fetchBashHelpersRepo
 alias bash_helpers_pull=pullBashHelpersRepo
