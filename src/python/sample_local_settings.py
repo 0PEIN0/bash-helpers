70bff83a56d1978e0903ba820ccfbfb68b64d951
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-05 22:20:23 +0600

    Updated sample local settings file for python scripts.

--- src/python/sample_local_settings.py
+++ src/python/sample_local_settings.py
@@ -1,6 +1,7 @@
 CONFIG = {
     'BASH_ALIASES_FILE_PATH': '~/bash-aliases.sh',
     'YOUTUBE_VIDEO_ID_LIST': [],
-    # A sample entry for `FILE_REPLACE_DEF_LIST` list: {'source': '/home/ubuntu/saved-files/local_settings.py','destination': '/home/ubuntu/project/local_settings.py', },
-    'FILE_REPLACE_DEF_LIST': [],
+    # A sample entry for `FILE_REPLACE_DEF_LIST_BEFORE_DAILY_OPERATION` or `FILE_REPLACE_DEF_LIST_AFTER_DAILY_OPERATION` list: {'source': '/home/ubuntu/saved-files/local_settings.py','destination': '/home/ubuntu/project/local_settings.py', },
+    'FILE_REPLACE_DEF_LIST_BEFORE_DAILY_OPERATION': [],
+    'FILE_REPLACE_DEF_LIST_AFTER_DAILY_OPERATION': [],
 }
