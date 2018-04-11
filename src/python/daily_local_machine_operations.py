8c6b63530874f1c966372888833e7330b889d349
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-11 17:12:03 +0600

    Added some more info print statements in python scripts.

--- src/python/daily_local_machine_operations.py
+++ src/python/daily_local_machine_operations.py
@@ -10,15 +10,23 @@ class DailyLocalMachineOperations(object):
 
     def __init__(self):
         if '-before-file-replace' in sys.argv:
+            print('INFO: pre-processing file replaces started.....')
             FileHelper().copy_and_replace_files(
                 file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST_BEFORE_DAILY_OPERATION'])
+            print('INFO: pre-processing file replaces completed.')
         if '-music-sync' in sys.argv:
+            print('INFO: music video file sync started.....')
             YoutubeMusicVideoSync().sync_mvs()
+            print('INFO: music video file sync completed.')
         if '-git-repos-sync' in sys.argv:
+            print('INFO: git repos sync started.....')
             GitOperations().fetch_all_repos_and_reset_hard()
+            print('INFO: git repos sync completed.')
         if '-after-file-replace' in sys.argv:
+            print('INFO: post-processing file replaces started.....')
             FileHelper().copy_and_replace_files(
                 file_path_def_list=LocalSettingsLoader().LOCAL_SETTINGS['FILE_REPLACE_DEF_LIST_AFTER_DAILY_OPERATION'])
+            print('INFO: post-processing file replaces completed.')
 
 
 DailyLocalMachineOperations()
