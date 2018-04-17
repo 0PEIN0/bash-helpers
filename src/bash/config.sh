62ba996807892be5885e18b712459aa1a7a831bf
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-17 15:00:54 +0600

    Isolated docker commands in a seperate shell file.

--- src/bash/config.sh
+++ src/bash/config.sh
@@ -8,6 +8,14 @@ if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/git.sh ]; then
     . $BASH_HELPER_GIT_FOLDER/src/bash/git.sh
 fi;
 
+if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/nodejs.sh ]; then
+    . $BASH_HELPER_GIT_FOLDER/src/bash/nodejs.sh
+fi;
+
+if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/docker.sh ]; then
+    . $BASH_HELPER_GIT_FOLDER/src/bash/docker.sh
+fi;
+
 if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/install.sh ]; then
     . $BASH_HELPER_GIT_FOLDER/src/bash/install.sh
 fi;
@@ -18,10 +26,6 @@ fi;
 
 if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/django.sh ]; then
     . $BASH_HELPER_GIT_FOLDER/src/bash/django.sh
-fi;
-
-if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/nodejs.sh ]; then
-    . $BASH_HELPER_GIT_FOLDER/src/bash/nodejs.sh
 fi;
 
 if [ -f $SYSTEM_ROOT_GIT_REPO_FOLDER/crugen/project-shell.sh ]; then
--- src/bash/install.sh
+++ src/bash/install.sh
@@ -764,38 +764,6 @@ installPhp() {
   goToRoot
 }
 
-installDocker() {
-  funcName=$(getFunctionName)
-  checkIfSudo $funcName
-  if [ "${?}" = "0" ] ; then
-    return
-  fi;
-  aptGet
-  cd $SYSTEM_SOFTWARE_FOLDER/
-  printf "y\n" | sudo apt-get remove docker docker-engine docker.io
-  printf "y\n" | sudo apt-get install apt-transport-https ca-certificates
-  printf "y\n" | sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo apt-key fingerprint 0EBFCD88
-  printf 'y\n' | sudo apt-get install docker-ce
-  #sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  #aptGet
-  # Dependent on ubuntu version
-  #printf '\n' | sudo add-apt-repository 'deb https://apt.dockerproject.org/repo ubuntu-"$(lsb_release -sc)" main'
-  #printf 'y\n' | sudo apt-get install docker-engine
-  #sudo apt-cache policy docker-engine
-  curl -sSL test.docker.com |sh
-  usermod -aG docker ${USER}
-  sudo adduser ${USER} docker
-  sudo gpasswd -a ${USER} docker
-  usermod -aG docker ${SYSTEM_USER_NAME}
-  sudo adduser ${SYSTEM_USER_NAME} docker
-  sudo gpasswd -a ${SYSTEM_USER_NAME} docker
-  newgrp docker
-  sudo service docker start
-  goToRoot
-}
-
 installHerokuToolbelt() {
   funcName=$(getFunctionName)
   checkIfSudo $funcName
@@ -1116,7 +1084,6 @@ alias install_blender=installBlender
 alias install_bracket="installBracket $LATEST_BRACKET_VERSION"
 alias install_clangd="installClangD $LATEST_CLANGD_VERSION"
 alias install_codeblocks=installCodeblocks
-alias install_docker="installDocker"
 alias install_golang="installGolang $LATEST_GOLANG_VERSION $LATEST_GOLANG_VERSION_FULL"
 alias install_hack_lang=installHackLang
 alias install_nginx=installNginx
