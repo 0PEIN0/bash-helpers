cc0926490131c9dd9e373879af1a12de5a2b7e0c
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-19 19:43:18 +0600

    Added installation commands for docker compose package.

--- src/bash/docker.sh
+++ src/bash/docker.sh
@@ -26,6 +26,8 @@ installDocker() {
   sudo adduser ${SYSTEM_USER_NAME} docker
   sudo gpasswd -a ${SYSTEM_USER_NAME} docker
   newgrp docker
+  sudo curl -L https://github.com/docker/compose/releases/download/$LATEST_DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
   sudo service docker start
   goToRoot
 }
