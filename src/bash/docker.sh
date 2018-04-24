76a899030a418ad6110d0f025c2e1149324c975d
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-24 14:20:37 +0600

    Placed the docker compose installation command in install docker method.

--- src/bash/docker.sh
+++ src/bash/docker.sh
@@ -28,6 +28,7 @@ installDocker() {
   newgrp docker
   sudo curl -L https://github.com/docker/compose/releases/download/$LATEST_DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
+  pip install --upgrade docker-compose
   sudo service docker start
   goToRoot
 }
