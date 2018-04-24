54a5b6229453fb15e99dd3998e5825eef3b2c56f
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-25 01:25:48 +0600

    Updated package versions and phantomjs installation method.

--- src/bash/package-versions.sh
+++ src/bash/package-versions.sh
@@ -1,18 +1,19 @@
-LATEST_PYCHARM_VERSION="pycharm-community-2018.1.1"
-LATEST_VSCODE_FILE_NAME="code_1.22.2-1523551015_amd64.deb"
-LATEST_SLACK_VERSION="3.1.1"
-LATEST_ROBOMONGO_VERSION="1.2.1"
-LATEST_ROBOMONGO_VERSION_FULL="robo3t-$LATEST_ROBOMONGO_VERSION-linux-x86_64-3e50a65"
-LATEST_PHANTOMJS_VERSION="2.1.1"
-LATEST_PHANTOMJS_VERSION_FULL="phantomjs-$LATEST_PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
-LATEST_GOLANG_VERSION="1.10.1"
-LATEST_GOLANG_VERSION_FULL="go$LATEST_GOLANG_VERSION.linux-amd64"
-LATEST_ANGRY_IP_SCAN_VERSION="3.5.2"
-LATEST_CLANGD_VERSION="5.0"
+LATEST_PYCHARM_VERSION="pycharm-community-2018.1.1" #https://www.jetbrains.com/pycharm/download/#section=linux
+LATEST_VSCODE_VERSION="1.22.2-1523551015" #https://code.visualstudio.com/download
+LATEST_BRACKET_VERSION="1.12" #https://github.com/adobe/brackets/releases
+LATEST_GOLANG_VERSION="1.10.1" #https://golang.org/dl/
+LATEST_SLACK_VERSION="3.1.1" #https://slack.com/downloads/linux
+LATEST_GEOS_VERSION="geos-3.6.2" #http://download.osgeo.org/geos/
+LATEST_POSTGIS_VERSION="postgis-2.4.4" #http://postgis.net/stuff/
+LATEST_PYTHON_VERSION="3.6.5" #https://www.python.org/downloads/
+LATEST_CLANGD_VERSION="5.0" #http://releases.llvm.org/download.html
+LATEST_DOCKER_COMPOSE_VERSION="1.21.0" #https://docs.docker.com/compose/install/
+LATEST_ROBOMONGO_VERSION="1.2.1" #https://robomongo.org/download
+LATEST_STACER_VERSION="1.0.9" #https://github.com/oguzhaninan/Stacer/releases
+LATEST_ANGRY_IP_SCAN_VERSION="3.5.2" #http://angryip.org/download/#linux
+LATEST_PHANTOMJS_VERSION="2.1.1" #https://bitbucket.org/ariya/phantomjs/downloads/
 
-LATEST_DOCKER_COMPOSE_VERSION="1.21.0"
-LATEST_GEOS_VERSION="geos-3.6.1"
-LATEST_POSTGIS_VERSION="postgis-2.3.3"
-LATEST_PYTHON_VERSION="3.6.5"
-LATEST_BRACKET_VERSION="1.12"
-LATEST_STACER_VERSION="1.0.6"
+LATEST_VSCODE_FILE_NAME="code_${LATEST_VSCODE_VERSION}_amd64.deb"
+LATEST_ROBOMONGO_VERSION_FULL="robo3t-$LATEST_ROBOMONGO_VERSION-linux-x86_64-3e50a65"
+LATEST_PHANTOMJS_VERSION_FULL="phantomjs-$LATEST_PHANTOMJS_VERSION-linux-x86_64"
+LATEST_GOLANG_VERSION_FULL="go$LATEST_GOLANG_VERSION.linux-amd64"
