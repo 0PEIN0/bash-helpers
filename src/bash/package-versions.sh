9e461f222098ebd452af4e0a419f2c9f03f99233
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-06-13 15:46:23 +0600

    Updated latest package versions.

--- src/bash/package-versions.sh
+++ src/bash/package-versions.sh
@@ -1,19 +1,19 @@
 LATEST_PYCHARM_VERSION="pycharm-community-2018.1.4" #https://www.jetbrains.com/pycharm/download/#section=linux
-LATEST_VSCODE_VERSION="1.23.1-1525968403" #https://code.visualstudio.com/download
+LATEST_VSCODE_VERSION="1.24.0-1528306776" #https://code.visualstudio.com/download
 LATEST_BRACKET_VERSION="1.12" #https://github.com/adobe/brackets/releases
-LATEST_GOLANG_VERSION="1.10.2" #https://golang.org/dl/
-LATEST_SLACK_VERSION="3.1.1" #https://slack.com/downloads/linux
+LATEST_GOLANG_VERSION="1.10.3" #https://golang.org/dl/
+LATEST_SLACK_VERSION="3.2.1" #https://slack.com/downloads/linux
 LATEST_GEOS_VERSION="geos-3.6.2" #http://download.osgeo.org/geos/
 LATEST_POSTGIS_VERSION="postgis-2.4.4" #http://postgis.net/stuff/
 LATEST_PYTHON_VERSION="3.6.5" #https://www.python.org/downloads/
 LATEST_CLANGD_VERSION="5.0" #http://releases.llvm.org/download.html
-LATEST_DOCKER_COMPOSE_VERSION="1.21.0" #https://docs.docker.com/compose/install/
+LATEST_DOCKER_COMPOSE_VERSION="1.21.2" #https://docs.docker.com/compose/install/
 LATEST_ROBOMONGO_VERSION="1.2.1" #https://robomongo.org/download
+LATEST_ROBOMONGO_VERSION_FULL="robo3t-$LATEST_ROBOMONGO_VERSION-linux-x86_64-3e50a65"
 LATEST_STACER_VERSION="1.0.9" #https://github.com/oguzhaninan/Stacer/releases
 LATEST_ANGRY_IP_SCAN_VERSION="3.5.2" #http://angryip.org/download/#linux
 LATEST_PHANTOMJS_VERSION="2.1.1" #https://bitbucket.org/ariya/phantomjs/downloads/
 
 LATEST_VSCODE_FILE_NAME="code_${LATEST_VSCODE_VERSION}_amd64.deb"
-LATEST_ROBOMONGO_VERSION_FULL="robo3t-$LATEST_ROBOMONGO_VERSION-linux-x86_64-3e50a65"
 LATEST_PHANTOMJS_VERSION_FULL="phantomjs-$LATEST_PHANTOMJS_VERSION-linux-x86_64"
 LATEST_GOLANG_VERSION_FULL="go$LATEST_GOLANG_VERSION.linux-amd64"
