82bafe0f1ed0e37750be104f7144df3e3906bff9
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-08-25 06:30:03 +0600

    Added support to install php 5.6 .

--- src/bash/php.sh
+++ src/bash/php.sh
@@ -28,10 +28,12 @@ installLaravelNonSudo() {
   if [ "${?}" = "0" ] ; then
     return
   fi;
+  goToRoot
   composer global require "laravel/installer"
   echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.bash_aliases
-  bash_refresh
+  eval "bash_refresh"
   #http://vaguelyuseful.info/2016/08/03/installing-laravel-5-2-on-ubuntu-16-04-and-apache2/
+  goToRoot
 }
 
 installPhpmyadmin() {
@@ -40,14 +42,30 @@ installPhpmyadmin() {
   if [ "${?}" = "0" ] ; then
     return
   fi;
+  goToRoot
   printf 'y\n' | sudo apt-get install mysql-server
   sudo mysql_secure_installation
   sudo apt-get install phpmyadmin php-mbstring php-gettext
   #https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-phpmyadmin-on-ubuntu-16-04
+  goToRoot
+}
+
+installPhpFive() {
+  funcName=$(getFunctionName)
+  checkIfSudo $funcName
+  if [ "${?}" = "0" ] ; then
+    return
+  fi;
+  goToRoot
+  printf '\n' | sudo add-apt-repository ppa:ondrej/php
+  aptGet
+  printf 'y\n' | sudo apt-get install -y php5.6
+  goToRoot
 }
 
 alias apache_restart='/etc/init.d/apache2 restart'
 alias get_apache_users='ps -ef | grep apache | grep -v grep'
 alias install_laravel=installLaravelNonSudo
 alias install_php=installPhp
+alias install_php_five=installPhpFive
 alias install_php_my_admin=installPhpmyadmin
